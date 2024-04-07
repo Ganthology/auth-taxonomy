96d8c911dddacddb780ec4f3f5ab1792cf110526
Ray Gan <ray@oyen.my>
2024-04-07 09:29:57 +0800

    build(prisma): add env validation

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,11 @@ import { z } from "zod"
 
 const envVariables = z.object({
   JWT_SECRET: z.string(),
+  DB_NAME: z.string(),
+  DB_HOST: z.string(),
+  DB_USER: z.string(),
+  DB_PASSWORD: z.string(),
+  DATABASE_URL: z.string(),
 })
 
 envVariables.parse(process.env)
